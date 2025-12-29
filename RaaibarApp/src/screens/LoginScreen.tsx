19962ffdfc21133ea82d10390f6ee456c3370573
mayyank1 <mayankgauur@gmail.com>
2025-12-29 18:16:52 +0530

    network error solved

--- RaaibarApp/src/screens/LoginScreen.tsx
+++ RaaibarApp/src/screens/LoginScreen.tsx
@@ -13,7 +13,7 @@ const LoginScreen = ({ navigation }: any) => {
 
         // Send login request to server(Authentication logic)
         try{
-            const response = await fetch('https://10.132.8.50/24/login',{
+            const response = await fetch('http://10.117.231.187:3000/login',{
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
